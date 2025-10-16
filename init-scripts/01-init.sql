3053a1c08c65e541ddc0579498ad09a3048fc35d
Kerem <aRandomEmail@kerem.com>
2025-10-16 08:28:27 -0400

    db update

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -3,8 +3,7 @@
 
 USE businessbeacon;
 
--- Example: Create a sample table
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS sellers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -12,8 +11,23 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS places (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    placeName VARCHAR(50) NOT NULL,
+    placeAddress VARCHAR(50) NOT NULL UNIQUE,
+    placeDescription VARCHAR(250) NOT NULL,
+    placeRating DOUBLE NOT NULL CHECK (placeRating >= 1 AND placeRating <= 10),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    placeLogoPath VARCHAR(255),
+    placeThumbnailPath VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    sellerID VARCHAR(150) NOT NULL
+);
+
 -- Example: Insert sample data
-INSERT INTO users (username, email) VALUES 
+INSERT INTO sellers (username, email) VALUES 
     ('admin', 'admin@example.com'),
     ('testuser', 'test@example.com')
 ON DUPLICATE KEY UPDATE username=username;
